0de64e98a84ba1b67228ed10a0dd4e81d2cebc53
“Adalmiina <adalmiinasuikkanen3@gmail.com>
2023-05-09 12:38:18 +0300

    feat: fetching one page of trips

---
+++ src/Service/TripInfo.js
@@ -0,0 +1,25 @@
+const apiUrl = process.env.REACT_APP_API_URL;
+
+/**
+ * Get user info with id
+ * @param {*} userId
+ * @returns [null, data] if ok, else [error.message, []]
+ */
+export const getOnePageOfTrips = async (page) => {
+    try {
+      const response = await fetch(`https://localhost:7183/GetOnePage?page=${page}`, {
+        headers: {
+          "Content-Type": "application/json"
+    
+        }
+    });
+      if (!response.ok) {
+        throw new Error("Could not complete request!");
+      }
+      const data = await response.json();
+      return [null, data];
+    } catch (error) {
+      return [error.message, []];
+    }
+  };
+  
